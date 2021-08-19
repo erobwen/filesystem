c72efa80e016176b594dd440e653466dfad730ae
Robert Wensman <robert.wensman@gmail.com>
2021-08-19 17:41:11 +0200

    Refactor select category dialog.

--- components/DesignExplorer/AddFolder.js
+++ components/DesignExplorer/AddFolder.js
@@ -16,6 +16,7 @@ import { ModalDialog, ModalPopover, Popover } from '../Popover';
 import { icons } from '../Icons';
 import { Button, LargeMenuItem, MenuItem } from '../Widgets';
 import { categories } from '../../application/model/Category';
+import { SelectCategoryDialog } from './SelectCategoryDialog';
 
 export function AddFolderPopover({open, close, boundingClientRect, explorerModel, openAddCategoryFolderDialog}) {
   let panelPadding = 5;
@@ -39,73 +40,23 @@ export function AddFolderPopover({open, close, boundingClientRect, explorerModel
   }}/>
 }
 
-function normalize(name) {
-  return name.toLowerCase().replace(/\s/g, "");
-}
 
 export function AddCategoryFolderDialog({open, close, explorerModel}) {
-  const [categoryName, setCategoryName] = useState("");
-  return <ModalDialog open={open} close={close} render={({style}) => {
-    const normalizedCategoryName = normalize(categoryName);
-
-    let exactMatch = null; 
-    const nonAvailable = {};
-    explorerModel.selectedFolder.addDirectChildCategoryFilters(nonAvailable);
-    explorerModel.selectedFolder.addAllIntersectedCategories(nonAvailable);
-    const availableCategories = []
-    categories.items.forEach(category => {
-      let name = normalize(category.name);
-      if (name.startsWith(normalizedCategoryName)
-          && !nonAvailable[category.id] && category.canBeFilter) {      
-        availableCategories.push(category);
-      }
-      if (name === normalizedCategoryName) {
-        exactMatch = category; 
-      }
-    });
-
-    let spacerSize = 15;
-    return (
-      <div style={{...columnStyle,...panelPaddingStyle, width: 500, height: 400, ...style}} 
-        onClick={(event) => {loge("click on column"); event.preventDefault();event.stopPropagation();}}>
-        <Text style={{fontSize: 16}}>Add Filter Folder</Text>
-        <Spacer size={spacerSize}/>
-        <Row style={{flexAutoStyle}} overflowVisible>
-          <Icon style={{marginRight: "0.5em"}} image={icons.object}/>
-          <Middle style={flexGrowShrinkAutoStyle}>
-            <TextInput style={{height: iconSize, lineHeight: iconSize}} onChangeText={setCategoryName} value={categoryName} autoFocus/>
-          </Middle>
-          <Button style={{...flexAutoWidthStyle(150), marginLeft: "0.5em"}} text={"Create new category"} 
-            onClick={() => { explorerModel.addFilterFolder(categoryName); close()}} 
-            disable={(exactMatch !== null) || categoryName.length === 0}/>
-        </Row>
-        <Spacer size={spacerSize}/>
-        <Text style={{fontSize: 16}}>Select a category</Text>
-        <Spacer size={spacerSize}/>
-        <CategorySelector availableCategories={availableCategories} selectCategory={(category) => {
-          explorerModel.addFilterFolder(category); close()
-        }}/>
-        {/* <CategoryNamePopover open={true}/> */}
-      </div>
-    )
-  }}/>
-}
-
-
-export function CategorySelector({selectCategory, availableCategories}) {
-
+  const nonAvailable = {};
+  explorerModel.selectedFolder.addDirectChildCategoryFilters(nonAvailable);
+  explorerModel.selectedFolder.addAllIntersectedCategories(nonAvailable);
+  function onSelect(categoryOrCategoryName) {
+    explorerModel.addFilterFolder(categoryOrCategoryName); close();
+  }
   return (
-    <Scroller>
-      <Column children={availableCategories.map(category =>  
-          <MenuItem
-            key={category.id}
-            image={category.image ? category.image : icons.object}
-            text={category.name}
-            onClick={() => {selectCategory(category)}}/>
-      )}/>
-    </Scroller>
+    <SelectCategoryDialog open={open} close={close} 
+      nonAvailable={nonAvailable} 
+      explorerModel={explorerModel}
+      onSelect={onSelect}
+      />
   );
 }
+
 
 // function CategoryNamePopover() {
 //   return <Popover open={open} close={close} bounds={{top: 100, left: 100, width: 100, height: 100}} render={({style, bounds}) => {
---
+++ components/DesignExplorer/SelectCategoryDialog.js
@@ -0,0 +1,86 @@
+import { StyleSheet, Text, View, TextInput, ScrollView } from 'react-native';
+import React, { Component, useState } from 'react';
+import { Column, columnStyle, fitStyle, flexAutoStyle, flexAutoWidthStyle, Flexer, flexGrowShrinkAutoStyle, Middle, pointerEventsAutoStyle, pointerEventsNoneStyle, Row, Wrapper, ZStack, zStackElementStyle, zStackStyle } from '../Layout';
+import { Icon } from '../Icon';
+
+import { ClickablePanel } from '../ClickablePanel';
+import { iconSize, panelBorderRightStyle, panelPadding, panelPaddingStyle, panelStyle, SelectionBase, sidePanelWidth, Spacer, transparentBlue, transparentGray } from '../Style';
+import { log, loge, logg } from '../utility/Debug';
+import { draggingType } from './DesignExplorer';
+import implyImage from '../../assets/imply.svg'
+import { Portal, PortalProvider, PortalHost } from '@gorhom/portal';
+
+import { DropTarget } from '../DropTarget';
+import { Scroller, scrollerContentStyle } from '../Scroller';
+import { ModalDialog, ModalPopover, Popover } from '../Popover';
+import { icons } from '../Icons';
+import { Button, LargeMenuItem, MenuItem } from '../Widgets';
+import { categories } from '../../application/model/Category';
+
+
+function normalize(name) {
+  return name.toLowerCase().replace(/\s/g, "");
+}
+
+export function SelectCategoryDialog({open, close, explorerModel, nonAvailable, onSelect}) {
+  const [categoryName, setCategoryName] = useState("");
+  return <ModalDialog open={open} close={close} render={({style}) => {
+    const normalizedCategoryName = normalize(categoryName);
+
+    let exactMatch = null;
+    const availableCategories = []
+    categories.items.forEach(category => {
+      let name = normalize(category.name);
+      if (name.startsWith(normalizedCategoryName)
+          && !nonAvailable[category.id] && category.canBeFilter) {      
+        availableCategories.push(category);
+      }
+      if (name === normalizedCategoryName) {
+        exactMatch = category; 
+      }
+    });
+
+    let spacerSize = 15;
+    return (
+      <div style={{...columnStyle,...panelPaddingStyle, width: 500, height: 400, ...style}} 
+        onClick={(event) => {loge("click on column"); event.preventDefault();event.stopPropagation();}}>
+        <Text style={{fontSize: 16}}>Add Filter Folder</Text>
+        <Spacer size={spacerSize}/>
+        <Row style={{flexAutoStyle}} overflowVisible>
+          <Icon style={{marginRight: "0.5em"}} image={icons.object}/>
+          <Middle style={flexGrowShrinkAutoStyle}>
+            <TextInput style={{height: iconSize, lineHeight: iconSize}} onChangeText={setCategoryName} value={categoryName} autoFocus/>
+          </Middle>
+          <Button style={{...flexAutoWidthStyle(150), marginLeft: "0.5em"}} text={"Create new category"} 
+            onClick={() => onSelect(categoryName)} 
+            disable={(exactMatch !== null) || categoryName.length === 0}/>
+        </Row>
+        <Spacer size={spacerSize}/>
+        <Text style={{fontSize: 16}}>Select a category</Text>
+        <Spacer size={spacerSize}/>
+        <CategorySelector 
+          availableCategories={availableCategories} 
+          selectCategory={(category) => onSelect(category)}/>
+        {/* <CategoryNamePopover open={true}/> */}
+      </div>
+    )
+  }}/>
+}
+
+
+
+
+export function CategorySelector({selectCategory, availableCategories}) {
+
+  return (
+    <Scroller>
+      <Column children={availableCategories.map(category =>  
+          <MenuItem
+            key={category.id}
+            image={category.image ? category.image : icons.object}
+            text={category.name}
+            onClick={() => {selectCategory(category)}}/>
+      )}/>
+    </Scroller>
+  );
+}
